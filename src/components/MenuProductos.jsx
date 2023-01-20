ac362fb8d1922101858cffc0099a733d274f3ec0
Ayelen Garcia <ayelengarcia7@hotmail.com>
2023-01-19 23:11:46 -0300

    edit json

--- src/components/MenuProductos.jsx
+++ src/components/MenuProductos.jsx
@@ -11,7 +11,6 @@ const MenuProductos = () => {
 
   const getProductos = () => {
     axios.get("../../products.json").then((response) => {
-      console.log(response.data);
       setProductos(response.data);
     });
   };
